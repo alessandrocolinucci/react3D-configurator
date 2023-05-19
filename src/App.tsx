ec2f1e02cdcb0fd78cd433271d0dcc6a848f6d40
Alessandro Colinucci <info@acolinucci.com>
2023-05-19 16:22:16 +0200

    Project initialized with Tailwind. Prettier config added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App = () => {
+  return <div className="flex items-center justify-center w-full h-full">
+    <div className="text-center">
+      <h1 className="underline text-3xl mb-4">3D Product Configurator</h1>
+      <p className="text-sm opacity-75">Made with React v.18 and ThreeJS</p>
+    </div>
+  </div>;
 }
 
-export default App
+export default App;
